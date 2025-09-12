da1c7063344aa916b4a0bdb8d5a26af0e2b2df6f
Owen <owen@txv.io>
2025-09-11 21:26:13 -0700

    Translate

--- src/app/[orgId]/settings/resources/create/page.tsx
+++ src/app/[orgId]/settings/resources/create/page.tsx
@@ -1423,7 +1423,7 @@ export default function Page() {
                                                 {t("resourceAddEntrypoints")}
                                             </h3>
                                             <p className="text-sm text-muted-foreground">
-                                                (Edit file: config/traefik/traefik_config.yml)
+                                                {t("resourceAddEntrypointsEditFile")}
                                             </p>
                                             <CopyTextBox
                                                 text={`entryPoints:
@@ -1438,7 +1438,7 @@ export default function Page() {
                                                 {t("resourceExposePorts")}
                                             </h3>
                                             <p className="text-sm text-muted-foreground">
-                                                (Edit file: docker-compose.yml)
+                                                {t("resourceExposePortsEditFile")}
                                             </p>
                                             <CopyTextBox
                                                 text={`ports:
