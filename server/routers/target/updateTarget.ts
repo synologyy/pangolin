22477b7e8161930da120d6ee98c8720eb41b101c
Pallavi Kumari <pallavilpmt1995@gmail.com>
2025-10-06 02:16:06 +0530

    add removed rewrite schema

--- server/routers/target/updateTarget.ts
+++ server/routers/target/updateTarget.ts
@@ -49,6 +49,8 @@ const updateTargetBodySchema = z
         hcStatus: z.number().int().optional().nullable(),
         path: z.string().optional().nullable(),
         pathMatchType: z.enum(["exact", "prefix", "regex"]).optional().nullable(),
+        rewritePath: z.string().optional().nullable(),
+        rewritePathType: z.enum(["exact", "prefix", "regex", "stripPrefix"]).optional().nullable(),
         priority: z.number().int().min(1).max(1000).optional(),
     })
     .strict()
