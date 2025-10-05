c7c3e3ee7393f3445a07262c9a63651bcd871c96
Pallavi Kumari <pallavilpmt1995@gmail.com>
2025-10-05 23:19:35 +0530

    refresh button inside admin

--- src/components/AdminUsersDataTable.tsx
+++ src/components/AdminUsersDataTable.tsx
@@ -9,11 +9,15 @@ import { useTranslations } from "next-intl";
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
     data: TData[];
+    onRefresh?: () => void;
+    isRefreshing?: boolean;
 }
 
 export function UsersDataTable<TData, TValue>({
     columns,
-    data
+    data,
+    onRefresh,
+    isRefreshing
 }: DataTableProps<TData, TValue>) {
 
     const t = useTranslations();
@@ -26,6 +30,8 @@ export function UsersDataTable<TData, TValue>({
             title={t('userServer')}
             searchPlaceholder={t('userSearch')}
             searchColumn="email"
+            onRefresh={onRefresh}
+            isRefreshing={isRefreshing}
         />
     );
 }
