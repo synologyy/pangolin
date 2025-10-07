7d0303e2beaa8d1cd7df41455a589bde38f9faa4
Owen <owen@txv.io>
2025-10-07 15:06:42 -0700

    Add postgres pool info to config

--- server/lib/readConfigFile.ts
+++ server/lib/readConfigFile.ts
@@ -158,7 +158,21 @@ export const configSchema = z
                             connection_string: z.string()
                         })
                     )
+                    .optional(),
+                pool: z
+                    .object({
+                        max_connections: z.number().positive().optional().default(20),
+                        max_replica_connections: z.number().positive().optional().default(10),
+                        idle_timeout_ms: z.number().positive().optional().default(30000),
+                        connection_timeout_ms: z.number().positive().optional().default(5000)
+                    })
                     .optional()
+                    .default({
+                        max_connections: 20,
+                        max_replica_connections: 10,
+                        idle_timeout_ms: 30000,
+                        connection_timeout_ms: 5000
+                    })
             })
             .optional(),
         traefik: z
