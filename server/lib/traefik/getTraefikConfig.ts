9d452efc7d3f996862995814efc719ab354cd370
Pallavi Kumari <pallavilpmt1995@gmail.com>
2025-10-15 19:52:31 +0530

    fix treafik config mismatch

--- server/lib/traefik/getTraefikConfig.ts
+++ server/lib/traefik/getTraefikConfig.ts
@@ -254,11 +254,7 @@ export async function getTraefikConfig(
             const domainCertResolver = resource.domainCertResolver;
             const preferWildcardCert = resource.preferWildcardCert;
 
-
-            let tls = {};
-            if (!privateConfig.getRawPrivateConfig().flags.generate_own_certificates) {
-
-                let resolverName: string | undefined;
+             let resolverName: string | undefined;
                 let preferWildcard: boolean | undefined;
                 // Handle both letsencrypt & custom cases
                 if (domainCertResolver) {
@@ -276,8 +272,7 @@ export async function getTraefikConfig(
                     preferWildcard = globalDefaultPreferWildcard;
                 }
 
-
-                tls = {
+                const tls = {
                     certResolver: resolverName,
                     ...(preferWildcard
                         ? {
@@ -289,7 +284,7 @@ export async function getTraefikConfig(
                         }
                         : {})
                 };
-            }
+            
 
             const additionalMiddlewares =
                 config.getRawConfig().traefik.additional_middlewares || [];
