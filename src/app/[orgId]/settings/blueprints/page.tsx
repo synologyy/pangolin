a5b48ab3924f48a914b12e6ed51056f0480ca693
Fred KISSIE <fredkiss3@gmail.com>
2025-10-23 00:13:31 +0200

    🚧 blueprints page

--- src/app/[orgId]/settings/blueprints/page.tsx
+++ src/app/[orgId]/settings/blueprints/page.tsx
@@ -1,6 +1,13 @@
-
-
-
+import SettingsSectionTitle from "@app/components/SettingsSectionTitle";
+import { internal } from "@app/lib/api";
+import { authCookieHeader } from "@app/lib/api/cookies";
+import OrgProvider from "@app/providers/OrgProvider";
+import { ListBlueprintsResponse } from "@server/routers/blueprints";
+import { GetOrgResponse } from "@server/routers/org";
+import { AxiosResponse } from "axios";
+import { getTranslations } from "next-intl/server";
+import { redirect } from "next/navigation";
+import { cache } from "react";
 
 
 type BluePrintsPageProps = {
@@ -8,7 +15,48 @@ type BluePrintsPageProps = {
     searchParams: Promise<{ view?: string }>;
 };
 
+
 export default async function BluePrintsPage(props: BluePrintsPageProps) {
     const params = await props.params;
-    return <></>
+
+    let blueprints: any[] = [];
+    try {
+        const res = await internal.get<
+            AxiosResponse<ListBlueprintsResponse>
+        >(`/org/${params.orgId}/domains`, await authCookieHeader());
+
+        blueprints = res.data.data.domains as any[];
+    } catch (e) {
+        console.error(e);
+    }
+
+    let org = null;
+    try {
+        const getOrg = cache(async () =>
+            internal.get<AxiosResponse<GetOrgResponse>>(
+                `/org/${params.orgId}`,
+                await authCookieHeader()
+            )
+        );
+        const res = await getOrg();
+        org = res.data.data;
+    } catch {
+        redirect(`/${params.orgId}`);
+    }
+
+    if (!org) {
+    }
+
+    const t = await getTranslations();
+    return (
+           <>
+               <OrgProvider org={org}>
+                   <SettingsSectionTitle
+                       title={t("blueprints")}
+                       description={t("blueprintsDescription")}
+                   />
+                   {/* <DomainsTable domains={domains} /> */}
+               </OrgProvider>
+           </>
+       );
 }
