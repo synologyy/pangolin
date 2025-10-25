4dd741cc3f919f2ee2b69faa570666643d06202e
Fred KISSIE <fredkiss3@gmail.com>
2025-10-25 02:55:19 +0200

    🔊 log all SQL queries

--- server/db/sqlite/driver.ts
+++ server/db/sqlite/driver.ts
@@ -13,12 +13,14 @@ bootstrapVolume();
 
 function createDb() {
     const sqlite = new Database(location);
-    return DrizzleSqlite(sqlite, { schema });
+    return DrizzleSqlite(sqlite, { schema, logger: true });
 }
 
 export const db = createDb();
 export default db;
-export type Transaction = Parameters<Parameters<typeof db["transaction"]>[0]>[0];
+export type Transaction = Parameters<
+    Parameters<(typeof db)["transaction"]>[0]
+>[0];
 
 function checkFileExists(filePath: string): boolean {
     try {
