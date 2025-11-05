030f90db2ec2ac8360d874350c6900fa00a269b3
Fred KISSIE <fredkiss3@gmail.com>
2025-11-05 21:41:29 +0100

    ♻️ validate `env` variables only in DEV

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 import { pullEnv } from "./src/lib/pullEnv";
-// validate env variables on build and such
-pullEnv();
+
+// validate env variables on local dev
+if (process.env.NODE_ENV === "development") {
+    pullEnv();
+}
 
 const withNextIntl = createNextIntlPlugin();
 
