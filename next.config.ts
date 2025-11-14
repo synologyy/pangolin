5b31bbce8de7834f5acb95ab479904a380724a08
miloschwartz <mschwartz10612@gmail.com>
2025-11-14 12:25:32 -0500

    remove frontend env parsing

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-import { pullEnv } from "./src/lib/pullEnv";
-
-// validate env variables on local dev
-if (process.env.NODE_ENV === "development") {
-    pullEnv();
-}
-
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
