a7185ff91349fd488cefcb830de704b466df0e12
miloschwartz <mschwartz10612@gmail.com>
2025-12-04 21:28:42 -0500

    add auth info tip

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -361,6 +361,15 @@ export default function LoginForm({
 
     return (
         <div className="space-y-4">
+            {forceLogin && (
+                <Alert variant="neutral">
+                    <AlertDescription className="flex items-center gap-2">
+                    <LockIcon className="w-4 h-4" />
+                        {t("loginRequiredForDevice")}
+                    </AlertDescription>
+                </Alert>
+            )}
+
             {showSecurityKeyPrompt && (
                 <Alert>
                     <FingerprintIcon className="w-5 h-5 mr-2" />
