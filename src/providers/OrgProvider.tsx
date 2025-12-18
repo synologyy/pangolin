1a976c78effadeb18c33d0b3b2bb9842fa91248a
Fred KISSIE <fredkiss3@gmail.com>
2025-12-18 04:27:24 +0100

    ♻️ separate org settings page into multiple forms

--- src/providers/OrgProvider.tsx
+++ src/providers/OrgProvider.tsx
@@ -10,36 +10,15 @@ interface OrgProviderProps {
     org: GetOrgResponse | null;
 }
 
-export function OrgProvider({ children, org: serverOrg }: OrgProviderProps) {
-    const [org, setOrg] = useState<GetOrgResponse | null>(serverOrg);
-
+export function OrgProvider({ children, org }: OrgProviderProps) {
     const t = useTranslations();
 
     if (!org) {
         throw new Error(t("orgErrorNoProvided"));
     }
 
-    const updateOrg = (updatedOrg: Partial<GetOrgResponse>) => {
-        if (!org) {
-            throw new Error(t("orgErrorNoUpdate"));
-        }
-
-        setOrg((prev) => {
-            if (!prev) {
-                return prev;
-            }
-
-            return {
-                ...prev,
-                ...updatedOrg
-            };
-        });
-    };
-
     return (
-        <OrgContext.Provider value={{ org, updateOrg }}>
-            {children}
-        </OrgContext.Provider>
+        <OrgContext.Provider value={{ org }}>{children}</OrgContext.Provider>
     );
 }
 
